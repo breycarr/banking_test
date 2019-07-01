e6795c5977982b13954f043663af51de038a0e92
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-07-01 16:05:37 +0100

    Feature added: Statement printing transactions are stored in memory and will be printed by calling statement the statement will be printed according to the appropriate format

--- lib/account.rb
+++ lib/account.rb
@@ -1,25 +1,43 @@
+require 'date'
+
 class Account
   STARTING_BALANCE = 0
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize(opening_balance = STARTING_BALANCE)
     @balance = opening_balance
+    @transactions = []
   end
 
-  def deposit(amount)
+  def deposit(amount, date =  Date.today)
     raise 'input must be a number' unless valid_number?(amount)
+
     @balance += amount
+    @transactions.push([date, amount, nil, @balance])
   end
 
-  def withdraw(amount)
+  def withdraw(amount, date = Date.today)
     raise 'input must be a number' unless valid_number?(amount)
+
     @balance -= amount
+    @transactions.push([date, nil, amount, @balance])
+  end
+
+  def statement
+    "date || credit || debit || balance
+    #{statement_format}"
   end
 
 private
 
   def valid_number?(amount)
-     amount.is_a?(Numeric)
+    amount.is_a?(Numeric)
+  end
+
+  def statement_format
+    statements = []
+    @transactions.each { |item| statements.push("#{item[0]} || #{item[1]} || #{item[2]} || #{item[3]}\n") }
+    statements.join
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,7 @@
 require 'account'
 
 describe Account do
-  subject = described_class.new
+  let(:subject) { described_class.new }
 
   describe '#show_balance' do
     it "shows the current balance in the account" do
@@ -11,19 +11,39 @@ describe Account do
 
   describe '#deposit' do
     it "accepts a nunber and increase the balance by that amount" do
-      expect{ subject.deposit(1) }.to change{ subject.balance }.by(1)
+      expect { subject.deposit(1) }.to change { subject.balance }.by(1)
+      p subject.transactions
     end
     it "should throw an error if the input is not a number" do
-      expect{ subject.deposit('one') }.to raise_error('input must be a number')
+      expect { subject.deposit('one') }.to raise_error('input must be a number')
     end
   end
 
   describe '#withdraw' do
     it "accepts a number and decreases the balance by that amount" do
-      expect{ subject.withdraw(1) }.to change{ subject.balance }.by(-1)
+      expect { subject.withdraw(1) }.to change { subject.balance }.by(-1)
     end
     it "should throw an error if the input is not a number" do
-      expect{ subject.withdraw('one') }.to raise_error('input must be a number')
+      expect { subject.withdraw('one') }.to raise_error('input must be a number')
+    end
+  end
+
+  describe '#statement' do
+    it "prints a formatted statement of their account" do
+      expect(subject.statement).to include("date || credit || debit || balance")
+    end
+    it "prints out a deposit in column 2" do
+      subject.deposit(1)
+      expect(subject.statement).to include("|| 1 ||  || 1")
+    end
+    it 'prints out a withdrawl in column 3' do
+      subject.deposit(10)
+      subject.withdraw(1)
+      expect(subject.statement).to include("||  || 1 || 9")
+    end
+    it "includes the date" do
+      subject.deposit("10/01/2012", 1000)
+      expect(subject.statement).to include("10/01/2012 || 1000 ||  || 1000")
     end
   end
 end
